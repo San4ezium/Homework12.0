dc45b17eb9193889a11984926ed48788b2518724
Oleksandr <sanyalisnyak@gmail.com>
2025-07-10 19:14:42 +0300

    add homework12.1

---
+++ homework12.1/homework12.1/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+int Fib(int n)
+{
+    if (n == 0)
+        return 0;
+
+    if (n == 1)
+        return 1;
+
+    return Fib(n - 1) + Fib(n - 2);
+}
+
+Console.Write("Enter the Fibonacci number: ");
+string input = Console.ReadLine();
+
+if (int.TryParse(input, out int n) && n >= 0)
+{
+    int result = Fib(n);
+    Console.WriteLine($"Fibonacci number at number {n}: {result}");
+}
+else
+{
+    Console.WriteLine("Please enter a non-negative integer.");
+}
+
